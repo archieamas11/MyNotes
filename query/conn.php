7983636808c644f4ffd5deaf91f2f37ca02e2ac3
Hariell Salmeron <hariellsalmeron@gmail.com>
2024-09-19 14:33:42 +0800

    updated

--- query/conn.php
+++ query/conn.php
@@ -1,10 +1,10 @@
 <?php
 
 // Database configuration using environment variables
-$servername = getenv('DB_HOST') 
-$username = getenv('DB_USERNAME')
-$password = getenv('DB_PASSWORD')
-$dbname = getenv('DB_DATABASE')
+$servername = getenv('DB_HOST') ;
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_DATABASE');
 $port = getenv('DB_PORT') ?: "3306";
 
 try {
